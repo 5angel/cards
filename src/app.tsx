a733128ca6a3b23dcca20a3af6b83b65ad5b415f
Vladislav Kozulya <vlad@dantebet.tech>
2025-03-03 17:39:47 +0200

    update

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,15 @@
-import Room from "./room";
+import Room from './room';
+
+const DOORS = [
+  1000, 100, 10, 1, 1100, 110, 11, 1001, 1101, 1110, 111, 1011, 1111,
+];
 
 export default function App() {
-    return (
-        <div className="container">
-            <Room doors={1} />
-            <Room doors={1} />
-        </div>
-    )
+  return (
+    <div className="container">
+      {DOORS.map((value) => (
+        <Room doors={value} />
+      ))}
+    </div>
+  );
 }
--- src/room.tsx
+++ src/room.tsx
@@ -1,16 +1,13 @@
 type RoomProps = {
-    doors: number;
-}
+  doors: number;
+};
 
 export default function Room({ doors }: RoomProps) {
-    console.log(doors, doors << 0)
-    return (
-        <div className="room">
-            <h3 className="room__title">Crossroads</h3>
-            { doors << 0 > 0 && <div className="door tp" />}
-            { doors << 1 > 0 && <div className="door rt" />}
-            { doors << 2 > 0 && <div className="door bt" />}
-            { doors << 3 > 0 && <div className="door lt" />}
-        </div>
-    )
+  const value = doors.toString(10).padStart(4, '0');
+  return (
+    <div className="room">
+      {/* <h3 className="room__title">Crossroads</h3> */}
+      <div className={`doors pos_${value}`}></div>
+    </div>
+  );
 }
