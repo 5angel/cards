507688f907326ec9e21369d285ad85da35d1d5b4
5angel <kozulyav@gmail.com>
2025-03-03 22:19:25 +0200

    update

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,9 @@
 import Room from './room';
 
-const DOORS = [
-  1000, 100, 10, 1, 1100, 110, 11, 1001, 1101, 1110, 111, 1011, 1111,
-];
-
 export default function App() {
   return (
     <div className="container">
-      {DOORS.map((value) => (
-        <Room doors={value} />
-      ))}
+        <Room doors={1111} />
     </div>
   );
 }
