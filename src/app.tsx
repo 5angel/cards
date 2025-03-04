a555bcce9798bc6ded3909d6bfcfce0a7981e9df
5angel <kozulyav@gmail.com>
2025-03-04 21:16:27 +0200

    wip

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,26 @@
-import Controls from './controls';
-import Room from './room';
+import { useState } from "react";
+import Room from "./room";
+import Controls from "./controls";
+
+type RoomConfig = {
+  doors: number;
+}
+
+function generateDoors() {
+  return parseInt(Math.max(1, Math.floor(Math.random() * 16)).toString(2), 10);
+}
 
 export default function App() {
+  const [doors, setDoors] = useState(generateDoors())
+
+  console.log(doors)
+
   return (
-    <div className="container">
-      <Controls />
-      <Room doors={1111} />
+    <div className="container" onClick={() => {
+      setDoors(generateDoors())
+    }}>
+        <Controls />
+        <Room doors={doors} />
     </div>
   );
 }
